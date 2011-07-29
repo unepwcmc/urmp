00ca81cf585c4c036d64b86a726fce9ce28cde86
James Cox <th3james@gmail.com>
2011-07-29 10:17:33 +0100

    Added rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Urmp::Application.config.secret_token = '890deb9b88abbad022cc3fe50f00240ccef1bb05746f8f3f7dc9620f0880160fff816fec2635ee1a3764df7dc18816be8f2c4028bbe2a9d5f6ea5dcd9e765c5e'
