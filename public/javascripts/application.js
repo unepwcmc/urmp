961f1fb50b9ee004a21fa37176f82f2fa92452e8
James Cox <th3james@gmail.com>
2011-09-02 12:24:29 +0100

    Added history state on opening panels

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,19 +3,28 @@
 
 (function() {
   var togglePrinciplePanel;
-  togglePrinciplePanel = function(element) {
-    var li = $(element).parent('li');
+  togglePrinciplePanel = function(li) {
+
     if (li.hasClass('active')) {
       li.find('div.block').slideUp();
       li.removeClass('active');
     } else {
       li.find('div.block').slideDown();
       li.addClass('active');
+
+      //Set history
+      var id = li.attr('id').split('principle-')[1];
+      var name = 'Principle ' + id;
+      if (id === '0'){
+        name = 'All Principles'
+      }
+      History.pushState({principle:id}, appName + " - " + name, "?priciple=" + id);
     }
   };
   $(document).ready(function() {
+    appName = $('title').html().split('Principle')[0];
     $('ul.slide-list').delegate('div.title-box', 'click', function() {
-      togglePrinciplePanel(this);
+      togglePrinciplePanel($(this).parent('li'));
       return false;
     });
   });
