406634b8088a2c26154623fedcfe4ce8659f595a
szynszyliszys <amjmigas@gmail.com>
2012-10-03 11:25:59 +0200

    Changed routes and adjusted css

--- app/controllers/static_controller.rb
+++
@@ -1,5 +0,0 @@
-class StaticController < ApplicationController
-  def about
-  end
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -59,7 +59,7 @@ Urmp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "principles#index"
+  root :to => "static_pages#home"
 
   # See how all your routes lay out with "rake routes"
 
