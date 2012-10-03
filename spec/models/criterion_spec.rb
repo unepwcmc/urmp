9ee0e9ccf09a5f1a64e60e49e8c342cdf77eafb5
Michal Wrobel <sparrovv@gmail.com>
2012-10-03 13:22:00 +0200

    Update specs and move factories

---
+++ spec/models/criterion_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe Criterion do
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :description }
+  it { should belong_to :principle }
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+require 'factory_girl'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -24,6 +25,7 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+  config.include FactoryGirl::Syntax::Methods
 
   # If true, the base class of anonymous controllers will be inferred
   # automatically. This will be the default behavior in future versions of
