5085f90d43dcb76aeed6427076b2a1979e8f440e
szynszyliszys <amjmigas@gmail.com>
2012-10-03 15:24:00 +0200

    Added static pages

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Urmp::Application.routes.draw do
 
   devise_for :users
 
+  root :to => "static_pages#home"
+  match "factsheets" => "static_pages#factsheets", :as => "factsheets"
+  match "monitoring" => "static_pages#monitorng", :as => "monitoring"
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -59,7 +63,6 @@ Urmp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "static_pages#home"
 
   # See how all your routes lay out with "rake routes"
 
