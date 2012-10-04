30190a83ee3b447810c8f99ed5ccf01e9aafa20e
Michal Wrobel <sparrovv@gmail.com>
2012-10-04 18:06:00 +0200

    Improve search

--- app/models/resource_search.rb
+++ app/models/resource_search.rb
@@ -2,7 +2,17 @@ class ResourceSearch
   include ActiveModel::Conversion
   extend ActiveModel::Naming
 
-  attr_accessor :text, :topic, :activity, :tool, :technical_expertise, :country, :language
+  SEARCH_ATTR = [:text, :topic, :activity, :tool, :technical_expertise, :country, :language]
+
+  attr_accessor *SEARCH_ATTR
+
+  def initialize(attrs={})
+    SEARCH_ATTR.each do |attr|
+      if val = (attrs[attr] || attrs[attr.to_s])
+        instance_variable_set("@#{attr.to_s}", val)
+      end
+    end
+  end
 
   def self.languages
     Resource.all.map(&:language).uniq
@@ -12,8 +22,12 @@ class ResourceSearch
     Resource.all.map(&:resource_type).uniq
   end
 
+  def search_text
+    "%#{text}%"
+  end
+
   def find
-    @results = Resource.where("")
+    @results = Resource.where("title like ? OR description like ?", search_text, search_text)
   end
 
   def results
