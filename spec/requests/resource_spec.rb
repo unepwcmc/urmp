16b8f1884c6cf7ba28a73904d4dccd100948d464
Michal Wrobel <sparrovv@gmail.com>
2012-10-05 12:33:39 +0200

    Add default_image to resource model

---
+++ spec/requests/resource_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe 'Resource' do
+
+  describe 'GET /resource/:id' do
+    let(:resource) {create(:resource) }
+
+    it 'displays information about resource' do
+      visit resource_path(resource)
+
+      page.should have_content resource.title
+      page.should have_content resource.description
+      page.should have_content resource.language
+      page.should have_content resource.institution
+      page.should have_selector 'a.external_link'
+    end
+  end
+end
