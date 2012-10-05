69b1f6d7f29203a000e2005a0a05dee2f812f0a1
Michal Wrobel <sparrovv@gmail.com>
2012-10-05 16:53:33 +0200

    Some basic styling for factsheets

--- app/helpers/factsheets_helper.rb
+++ app/helpers/factsheets_helper.rb
@@ -1,2 +1,9 @@
 module FactsheetsHelper
+  def name_for_theme(theme)
+    {
+      'impacts' => 'Monitoring of REDD+ impacts of biodiversity and ecosystem services',
+      'identification' => 'Identification, mapping, and valuation of biodiveristy and ecoxystem services',
+      nil => 'All the rest'
+    }[theme]
+  end
 end
