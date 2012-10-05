a165cda654e9e4591590ea374f9e253c6c098733
Michal Wrobel <sparrovv@gmail.com>
2012-10-05 15:00:28 +0200

    10 results per page

--- app/models/resource_search.rb
+++ app/models/resource_search.rb
@@ -7,20 +7,23 @@ class ResourceSearch
   include URMP::SelectableFields
 
   SEARCH_ATTR = [:text, :theme, :activity, :resource_type, :technical_expertise, :country, :language]
+  RESULTS_PER_PAGE = 10
 
   attr_accessor *SEARCH_ATTR
-  attr_reader :page
+  attr_reader :page, :results_per_page
 
   selectable_fields [:language, :theme, :resource_type, :country, :topic]
 
   def initialize(attrs={}, page=1)
+    @page = page
+    @results_per_page = RESULTS_PER_PAGE
+
     attrs = attrs || {}
     SEARCH_ATTR.each do |attr|
       if val = (attrs[attr] || attrs[attr.to_s])
         instance_variable_set("@#{attr.to_s}", val)
       end
     end
-    @page = page
   end
 
   def find
@@ -30,7 +33,7 @@ class ResourceSearch
       "resource_type_eq" => resource_type,
       "theme_eq" => theme,
       "country_eq" => country
-    ).page(page).per(15)
+    ).page(page).per(results_per_page)
   end
 
   def results
