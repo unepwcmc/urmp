150ebfe2d95ebb460048df9fa75661cf39dcd6ee
Michal Wrobel <sparrovv@gmail.com>
2012-10-09 10:03:52 +0200

    Add integration spec for search

--- spec/requests/resource_search_spec.rb
+++ spec/requests/resource_search_spec.rb
@@ -12,4 +12,23 @@ describe 'Search' do
     it_should_behave_like :succesful_page
   end
 
+  describe 'search' do
+    let(:resource_1) { create(:resource, :title => 'foo biz')}
+    let(:resource_2) { create(:resource, :title => 'bar')}
+
+    it 'finds searched results' do
+      visit search_path
+      fill_in "search_text", :with => resource_1.title
+
+      click_on 'Search'
+
+      within '.results' do
+        page.should have_content resource_1.title
+        page.should have_content resource_1.description
+
+        page.should_not have_content resource_2.title
+      end
+    end
+  end
+
 end
